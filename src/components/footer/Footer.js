cf64d2e7dffbff9b4c210d9d7a7fd74e839e42e9
legendof17 <legendof17gta@gmail.com>
2022-03-28 01:02:56 +0530

    added nickname to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,6 +10,7 @@ export default function Footer(props) {
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
           Made with <span role="img">❤️</span> by {greeting.title}
+          <br />({greeting.nickname})
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
